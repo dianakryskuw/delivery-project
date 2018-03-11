e9a6e33ea48fbee3b28d17ceaa49eb7a30e004fb
anna41 <annasapuzhak94@example.com>
2018-03-11 16:23:41 +0200

    added my code

---
+++ server/controllers/schema.js
@@ -0,0 +1,42 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+mongoose.Promise = global.Promise;
+var orderScheme = new Schema({
+    departure_point: {
+        lat: Number,
+        lng: Number,
+        address: String
+    },
+    arrival_point: {
+      lat: Number,
+      lng: Number,
+      address: String
+    },
+    time: {
+    text: String,
+    value: Number
+  },
+    distance: Number,
+    price: Number,
+    date:Date,
+    arrivalDate: { type: Date, default: null },
+    email: String,
+    status:{
+      type: String,
+      enum: ['in the store', 'on the way','delivered']},
+    items: [
+        {
+            name: String,
+            weight: Number
+        }
+    ]
+  })
+  
+  var carScheme = new Schema({
+    status:{ type: Boolean, default: false },// true-car is busy, false-car is empty
+    capacity:{ type: Number, default: 1 },
+    order_id:{ type: String, default: null },
+    finish_time:{ type: Date, default: null },
+  })
+
+  module.exports={orderScheme,carScheme}
